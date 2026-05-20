817566db0fc620f1826d5c5b6037e24233df94e9
Guilherme Quintiliano <guilhermehquintiliano@gmail.com>
2026-05-20 20:29:56 -0300

    docs: exercicios de python

--- exp1/calculadora_fatorial.py
+++ exp1/calculadora_fatorial.py
@@ -2,10 +2,12 @@
 
 import math
 
+
+# Captura o valor digitado pelo usuário e converte de string para inteiro usando int()
 numero_usuario = int(input("Digite o número para fatoriar: "))
 
 if numero_usuario < 0:
     print("Fatorial não existe para números negativos!")
 else:
-    resultado = math.factorial(numero_usuario)
-    print(f"Resultado da fatoriação de {numero_usuario}! é: {resultado}")
+    resultado_fatorial = math.factorial(numero_usuario)
+    print(f"Resultado da fatoriação de {numero_usuario}! é: {resultado_fatorial}")
--- exp1/conversor_temperatura.py
+++ exp1/conversor_temperatura.py
@@ -1,7 +1,7 @@
 #conversor_de_temperatura
 
-celsius = float(input("Digite a temperatura: ")) 
-print(f"Sua temperatura em celsius é: {celsius:.2f} °C")
+temperatura_celsius = float(input("Digite a temperatura: ")) 
+print(f"Sua temperatura em celsius é: {temperatura_celsius:.2f} °C")
 
-fahrenheit = (celsius * 9 / 5) + 32
-print(f"Sua temperatura em fahrenheit é: {fahrenheit:.2f} °F")
+temperatura_fahrenheit = (temperatura_celsius * 9 / 5) + 32
+print(f"Sua temperatura em fahrenheit é: {temperatura_fahrenheit:.2f} °F")
